b170c49ba5d3ce0b141eb0aa10a2b72ca14d90f4
Ekaterina Chegrinets <chegrinets.ekaterina@gmail.com>
2018-03-13 21:22:19 +0200

    refactoring index.js

--- index.js
+++ index.js
@@ -7,25 +7,23 @@ const readFile = util.promisify(fs.readFile);
 const compareWords = require('./lib/compare-words/index');
 const format = require('./lib/format');
 
-const argv = process.argv.slice(-2);
-const filePath = __dirname;
-const directory = 'files';
-const firstFilePath = filePath + '/' + directory + '/' + argv[0];
-const secondFilePath = filePath + '/' + directory + '/' + argv[1];
+const [firstFileName, secondFileName] = process.argv.slice(-2);
+const firstFilePath = `${__dirname}/files/${firstFileName}`;
+const secondFilePath = `${__dirname}/files/${secondFileName}`;
 
 const compareFiles = async (firstFilePath, secondFilePath) => {
   try {
-    const firstFileData = await readFile(firstFilePath, "utf8");
-    const secondFileData = await readFile(secondFilePath, "utf8");
+    const firstFileData = await readFile(firstFilePath, 'utf8');
+    const secondFileData = await readFile(secondFilePath, 'utf8');
 
     const data = compareWords(firstFileData, secondFileData);
 
     return format(data);
   } catch (error) {
-    throw new Error('Invalid request');
+    throw new Error('Invalid input params');
   }
 };
 
 compareFiles(firstFilePath, secondFilePath)
-  .then(result => process.stdout.write(result))
-  .catch(error => console.error(error));
+  .then(console.log)
+  .catch(console.error);
